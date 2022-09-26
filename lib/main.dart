a4dd568d27fe83acf0e3caaefa0aa53396fe0acb
fvieir <fcvieira2@gmail.com>
2022-09-26 17:39:03 -0300

    Adicionando perguntas e botões de resposta com ações ao precionar botões

--- lib/main.dart
+++ lib/main.dart
@@ -1,1 +1,46 @@
+import 'package:flutter/material.dart';
 
+void main() {
+  runApp(const PerguntaApp());
+}
+
+class PerguntaApp extends StatelessWidget {
+  const PerguntaApp({super.key});
+
+  void responder() {
+    print('Pergunta respondida');
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    final perguntas = [
+      'Qual é a sua cor favorita?',
+      'Qual é o seu animal favorito?',
+    ];
+
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text('Perguntas'),
+        ),
+        body: Column(
+          children: [
+            Text(perguntas[0]),
+            ElevatedButton(
+              onPressed: responder,
+              child: const Text('Resposta 1'),
+            ),
+            ElevatedButton(
+              onPressed: responder,
+              child: const Text('Resposta 2'),
+            ),
+            ElevatedButton(
+              onPressed: responder,
+              child: const Text('Resposta 3'),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
