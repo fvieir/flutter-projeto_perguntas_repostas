855ca21826eae2a6bbd1b1c937eb1d69e8a76520
fvieir <fcvieira2@gmail.com>
2022-09-27 10:55:31 -0300

    Adicionando Widget Questão

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'questao.dart';
 
 void main() {
   runApp(const PerguntaApp());
@@ -28,7 +29,7 @@ class _StatePerguntaApp extends State<PerguntaApp> {
         ),
         body: Column(
           children: [
-            Text(perguntas[_perguntaRespondida]),
+            Questao(perguntas[_perguntaRespondida]),
             ElevatedButton(
               onPressed: _responder,
               child: const Text('Resposta 1'),
